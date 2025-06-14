b6bf3bd24eff2ec61a2dc2b3a6ab86e73600ab35
+ <rgigs123@gmail.com>
2025-06-14 05:41:58 +0300

    complete arithmetic and random tests in index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Create num1 and num2 and multiply them to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Create num3 and num4, and get remainder 4
+const num3 = 16;
+const num4 = 12;
+const mod = num3 % num4;
+
+// Get the highest number from a set — should be 20
+const max = Math.max(5, 10, 15, 20);
